8ffd5b7829e2ca2e364a711fcb1af36a3adf5477
JordiTrassi <j.trassi@telefonica.net>
2022-08-05 17:05:15 +0200

    Installed React Vite environment

---
+++ music-app/src/MusicApp.jsx
@@ -0,0 +1,11 @@
+
+
+export const MusicApp = () => {
+  return (
+    <>
+      <h1>Music App with iTunes API</h1>
+    </>
+  )
+}
+
+
---
+++ music-app/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MusicApp } from './MusicApp';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MusicApp />
+  </React.StrictMode>
+);
